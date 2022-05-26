6588646fb2e6dbae7340c3c44becabaa26f3b68d
avinicius19 <101371607+avinicius19@users.noreply.github.com>
2022-05-25 21:54:53 -0300

    Add files via upload

---
+++ AULA 10/ProgramaPrincipal.java
@@ -0,0 +1,17 @@
+
+public class ProgramaPrincipal {
+    public static void main(String[] args) {
+        int num = ConsoleUtils.lerInteiro("Digite um inteiro:");
+        try {
+            long fat = Fatorial.calcular(num);
+            System.out.printf("%d! = %d", num, fat);
+        } catch(IllegalArgumentException ex){
+            System.out.println(ex.getMessage());
+        } finally {
+            System.out.println("\nPrograma terminado. Volte sempre");
+        }
+              
+    }
+    
+}
+
